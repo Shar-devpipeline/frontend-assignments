be3051c1a53f8bfa550af535b47a9604b564570e
Shar Hess <sharhess@dpadmins-MacBook-Pro.local>
2024-10-22 14:44:15 -0600

    conditional-assignment

--- javascript/conditionalsjs/conditionals.js
+++ javascript/conditionalsjs/conditionals.js
@@ -14,6 +14,9 @@ objectThree = Object.assign({}, objectOne, objectTwo);
 
 console.log(objectThree);
 
+// Copies properties from a source object to a target object
+// Object.assign(target, source)
+
 // ASSIGNMENT NUMBER 2
 
 function largestNum(numOne, numTwo) {
@@ -26,7 +29,15 @@ function largestNum(numOne, numTwo) {
   }
 }
 
-largestNum(15, 15);
+largestNum(9, 15);
+
+function largestNum(numOne, numTwo) {
+  numOne > numTwo
+    ? console.log(`${numOne} is the largest`)
+    : numOne < numTwo
+    ? console.log(`${numTwo} is the largest`)
+    : console.log("They are equal");
+}
 
 // ASSIGNMENT NUMBER 3
 
@@ -46,12 +57,12 @@ titleCase("hello from all of us crazy people");
 
 function toAlpha(str) {
   const arrayOne = str.split("");
-  result = arrayOne
-    .sort()
-    .map((char) => char.toUpperCase())
-    .join("");
-  replacews = result.replace(/\s+/g, "");
-  return replacews;
+
+  result = arrayOne.sort().join("");
+
+  whitesp = result.replace(/\s+/g, "");
+
+  return whitesp.toUpperCase();
 }
 
 console.log(toAlpha("this is a really long sentence lets see how it does"));
